6121b2d89618567cbdfb566852a5d7b6cca2c9ba
maffinca69 <35261681+maffinca69@users.noreply.github.com>
2024-10-30 18:06:58 +0400

    Update evtwebsocket.go

--- evtwebsocket.go
+++ evtwebsocket.go
@@ -53,7 +53,7 @@ func (c *Conn) Dial(url, subprotocol string) error {
 		defer c.close()
 
 		for {
-			var msg []byte
+			var msg = make([]byte, 1000000)
 			var n int
 			if n, err = c.ws.Read(msg); err != nil {
 				if c.OnError != nil {
